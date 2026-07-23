79bb3ae2e3b7557ecd2381643a62c9a0369a6676
leomth13 <117120804+leomth13@users.noreply.github.com>
2026-07-23 22:00:02 +0200

    Delete __init__.py for Fullup V2 Integration
    Removed the entire content of the __init__.py file for the Fullup V2 Integration.

--- custom_components/fullup/__init__.py
+++ custom_components/fullup/__init__.py
@@ -1,16 +1,1 @@
-"""Fullup V2 Integration"""
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.typing import ConfigType
 
-DOMAIN = "fullup"
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    return True
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
-    return True
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
